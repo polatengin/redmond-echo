d6a1fb3545655e3b28edd75ee0980fd228d7ce8b
polatengin <enpolat@microsoft.com>
2024-09-03 09:59:36 -0700

    implementing -n option to suppress newline

--- src/main.c
+++ src/main.c
@@ -3,14 +3,26 @@
 
 int main(int argc, char *argv[])
 {
+  int newline = 1;
   for (int i = 1; i < argc; i++)
   {
+    if (strcmp(argv[i], "-n") == 0)
+    {
+      newline = 0;
+    }
+    else
+    {
       printf("%s", argv[i]);
       if (i < argc - 1)
       {
         printf(" ");
       }
+    }
+  }
+  if (newline)
+  {
     printf("\n");
   }
+
   return 0;
 }
